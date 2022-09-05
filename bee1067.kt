4c9458ee827c4cd86a5a7094ac83ce8003e4b613
Márcio <102703578+marciogui12@users.noreply.github.com>
2022-09-05 12:47:37 +0000

    Create bee1067.kt

---
+++ bee1067.kt
@@ -0,0 +1,21 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val ler = Scanner(System.`in`)
+    val inteiro = ler.nextInt()
+    var i = 1
+
+    while(i<=inteiro){
+        println(i)
+        i = i+2
+
+
+
+
+
+
+    }
+
+
+
+    }
